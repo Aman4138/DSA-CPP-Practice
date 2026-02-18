5fa448673fd1f56ab01e514723cfa061f3d6a703
Amrendra kumar <143737212+Aman4138@users.noreply.github.com>
2026-02-18 17:40:32 +0530

    Create stringsize vs_length.cpp

---
+++ Basics/stringsize vs_length.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "HelloWorld";
+
+    cout << "String: " << s << endl;
+    cout << "size(): " << s.size() << endl;
+    cout << "length(): " << s.length() << endl;
+    return 0;
+}
